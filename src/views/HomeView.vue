927b495498231bb84242b9fa308b7e02d2884c6a
keithb1525 <keithb1525@gmail.com>
2025-01-27 16:11:18 -0500

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<script>
+
+export default {
+  data(){
+    return {
+      slogan: ["'Creative problem solver'", "'Innovative'", "'A team player'"]
+    }
+  },
+  mounted() {
+    this.rotateText()
+  },
+  methods:{
+    rotateText(){
+      window.setInterval(()=>{
+        const first = this.slogan.shift();
+        this.slogan = this.slogan.concat(first);
+    }, 5200);
+    }
+  }
+
+}
+
+  
+
+</script>
+
+<template>
+<v-container>
+  <v-row class="fade-in text-center">
+    <p>Hello World, my name is Keith.</p>
+  </v-row>
+   <v-row>
+   <h1>I just enjoy making creative solutions.</h1>
+   </v-row>
+   <v-row>
+   <h3 id="sloText" class="fade-in-text-rotate"> {{slogan[0]}} </h3>
+   </v-row>
+ 
+</v-container>
+</template>
---
+++ src/views/PortfolioView.vue
@@ -0,0 +1,38 @@
+<script setup>
+import ProjectPreview from '../components/ProjectPreview.vue'
+</script>
+<template>
+    <div class="about fade-in">
+      <v-row>
+      <v-col cols="12" md="3">
+        <ProjectPreview imageURL="/assets/project1.png" title="GPS Tracker" route="/" description="coming soon..."/>
+    </v-col>
+    <v-col cols="12" md="3">
+        <ProjectPreview imageURL="/assets/project1.png" title="DynamoDB Example" route="/dynamodb" description="coming soon..."/>
+    </v-col>
+    <v-col cols="12" md="3">
+        <ProjectPreview imageURL="/assets/project1.png" title="API Fetcher" route="/" description="coming soon..."/>
+    </v-col>
+    <v-col cols="12" md="3">
+        <ProjectPreview imageURL="/assets/project1.png" title="Website Revamp" route="/" description="coming soon..."/>
+    </v-col>
+    <v-col cols="12" md="3">
+        <ProjectPreview imageURL="/assets/project1.png" title="Status Page Example (Health Checks)" route="/" description="coming soon..."/>
+    </v-col>
+    <v-col cols="12" md="3">
+        <ProjectPreview imageURL="/assets/project1.png" title="coming soon..." route="/" description="coming soon..."/>
+    </v-col>
+    <v-col cols="12" md="3">
+        <ProjectPreview imageURL="/assets/project1.png" title="coming soon..." route="/" description="coming soon..."/>
+    </v-col>
+    <v-col cols="12" md="3">
+        <ProjectPreview imageURL="/assets/project1.png" title="coming soon..." route="/" description="coming soon..."/>
+    </v-col>
+  </v-row>
+    </div>
+  </template>
+  
+  <style>
+  
+  </style>
+  
