5aa6a6d3abaa6d0b62a35f17fc11dd5be71aca24
keithb1525 <keithb1525@gmail.com>
2025-01-31 12:27:35 -0500

    wip

--- src/views/RateCalculatorView.vue
+++ src/views/RateCalculatorView.vue
@@ -8,66 +8,68 @@ import { mapStores } from 'pinia';
 export default {
     data() {
         return {
-
             items: [],
-            selected: {text: "3 Month CD - 4.50%", val: 4.5, term: 3},
+            selected: { text: "3 Month CD - 4.50%", val: 4.5, term: 3 },
             itemquery: [],
-            rates:  [ 
-            { text: "3 Month CD - 4.50%", val: 4.5, term: 3 },
-            { text: "6 Months CD - 4.50%", val: 4.5, term: 6 },
-            { text: "12 Months CD - 3.75%", val: 3.75, term: 12 },
-            { text: "18 Months CD - 1.5%", val: 1.5, term: 18 },
+            rates: [
+                { text: "3 Month CD - 4.50%", val: 4.5, term: 3 },
+                { text: "6 Months CD - 4.50%", val: 4.5, term: 6 },
+                { text: "12 Months CD - 3.75%", val: 3.75, term: 12 },
+                { text: "18 Months CD - 1.5%", val: 1.5, term: 18 },
             ],
             principal: 25000,
             totalEarn: 0,
             leader: 75,
-            bank1: 50,
-            bank2: 35,
-            bank3: 25, 
-
+            bank1: 53,
+            bank2: 45,
+            bank3: 67,
         }
     },
     mounted() {
-         this.items = this.portfolioStore.bankItems;
-        // this.itemquery = this.portfolioStore.productquery[0];
-        // this.calcSave();
+        this.items = this.portfolioStore.bankItems;
     },
 
     computed: {
         ...mapStores(usePortfolioStore),
-
-        formattedValue: {
-            get () {
-                return this.principal.toLocaleString();
-            },
-            set (newValue){
-                this.principal = Number(newValue.replace(/,/g, ''));
-            }
-        }
     },
-    methods:{
-        calcEarn (rate, upe){
-            let t = this.selected.term/12;
-            let r = rate /100;
+
+    methods: {
+
+        calcEarn(rate, upe) {
+            let t = this.selected.term / 12;
+            let r = rate / 100;
             let p = this.principal;
             let n = 12;
             let base = 1 + (r / n);
-            // Calculate the exponent
             let exponent = n * t;
-            // Calculate A using the Math.pow() function
             let a = p * Math.pow(base, exponent);
             let ab = (a - p).toLocaleString('en-US', {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
             })
-            if (upe > 0){
-            this.totalEarn = ab;
+            if (upe > 0) {
+                this.totalEarn = ab;
+
             }
             return ab
         },
 
+        updateGraph() {
+            let total = parseFloat(this.totalEarn.replace(/,/g, "")) * 1.35;
+            let lead = parseFloat(this.totalEarn.replace(/,/g, ""));
+            let mega = parseFloat(document.getElementById("Mega").textContent.replace(/[\$,]/g, ""));
+            let local = parseFloat(document.getElementById("Local").textContent.replace(/[\$,]/g, ""));
+            let digi = parseFloat(document.getElementById("Digital").textContent.replace(/[\$,]/g, ""));
 
-    
+            this.leader = lead / total * 100;
+            this.bank1 = mega / total * 100;
+            this.bank2 = local / total * 100;
+            this.bank3 = digi / total * 100;
+
+
+        }
+
+
     }
 }
 </script>
@@ -88,29 +90,24 @@ export default {
                     <v-row class="mx-0">
                         <v-col col="6" class="rate-select">
                             <p>Select the product you're interested in</p>
-                            <v-select
-                                :items="rates"
-                                item-title="text"
-                                item-value="val"
-                                v-model="selected"
-                                variant="outlined"
-                                return-object
-                                ></v-select>
+                            <v-select :items="rates" item-title="text" item-value="text" v-model="selected"
+                                variant="outlined" @update:model-value="updateGraph()" return-object></v-select>
                         </v-col>
                         <v-col cols="6">
                             <p>Enter an estimate for the balance</p>
-                            <v-text-field prefix="$" v-model="formattedValue" variant="outlined"></v-text-field>
+                            <v-text-field prefix="$" v-model="principal" type="number" variant="outlined"
+                                hide-spin-buttons @input="updateGraph()"></v-text-field>
 
                         </v-col>
                     </v-row>
                     <v-row class="mx-0">
                         <v-col>
-                            <p>Based on this information, you would earn <span class="fw-bold"
-                                    style="color:#9f2d20">${{ totalEarn }}</span> with a Leader Bank CD than at the
+                            <p>Based on this information, you would earn <span class="fw-bold" style="color:#9f2d20">${{
+                                totalEarn }}</span> with a Leader Bank CD than at the
                                 average mega bank.</p>
                         </v-col>
                     </v-row>
-                    <v-row class="mx-0" >
+                    <v-row class="mx-0">
                         <v-col cols="12" md="6" class="table-col">
                             <v-table fixed-header height="300px">
                                 <thead>
@@ -135,7 +132,7 @@ export default {
                                     <tr v-for="(item, index) in items" :key="item.name">
                                         <td class="fw-bold">{{ item.name }}</td>
                                         <td>{{ item.rate }}%</td>
-                                        <td>${{ calcEarn(item.rate) }}</td>
+                                        <td v-bind:id="item.name">${{ calcEarn(item.rate) }}</td>
                                     </tr>
                                 </tbody>
                             </v-table>
@@ -146,7 +143,7 @@ export default {
                             <br>
 
                             <v-progress-linear v-model="bank1" color="#ffad31" height="25">
-                            
+
                             </v-progress-linear>
                             <div style="color:#ffad31;font-weight:bold">Mega</div>
 
@@ -154,13 +151,13 @@ export default {
                             <br>
 
                             <v-progress-linear v-model="bank2" color="#1e5895" height="25">
-                               
+
                             </v-progress-linear>
                             <div style="color:#1e5895;font-weight:bold">Local</div>
                             <br>
 
                             <v-progress-linear v-model="bank3" color="green" height="25">
-                          
+
                             </v-progress-linear>
                             <div style="color:#1dbf73;font-weight:bold">Digital</div>
                         </v-col>
@@ -173,39 +170,48 @@ export default {
     </v-container>
 </template>
 <style lang="scss">
-
-.rate-select{
-    .v-field__input{
+.rate-select {
+    .v-field__input {
         width: unset !important;
     }
 }
+
+.v-progress-linear__determinate {
+    border-top-right-radius: 20px;
+    border-bottom-right-radius: 20px;
+}
+
 .table-col {
     table {
         border: solid 1px #DDE1E6;
         border-radius: 10px;
+
         thead {
             background-color: #9f2d20;
-            color:white;
+            color: white;
+
             tr {
                 th {
                     background: none !important;
                 }
             }
         }
+
         th:first-of-type {
             border-top-left-radius: 10px;
         }
+
         th:last-of-type {
             border-top-right-radius: 10px;
         }
+
         tr:last-of-type td:first-of-type {
             border-bottom-left-radius: 10px;
         }
+
         tr:last-of-type td:last-of-type {
             border-bottom-right-radius: 10px;
-        }               
+        }
     }
 }
-   
-
 </style>
